d191d7f3ddc8e50a06b6c32ab07c0cb47bc4e888
Sam Phippen <samphippen@googlemail.com>
2012-08-25 14:22:28 +0100

    Add the abstract statusmodifier class
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/StatusModifier.java
@@ -0,0 +1,21 @@
+package me.teaisaweso.games.ld24;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public abstract class StatusModifier {
+    public double adjustSpeed(double speed) {
+        return speed;
+    }
+    
+    public double adjustCurrentSpeed(double currentSpeed) {
+        return this.adjustSpeed(currentSpeed);
+    }
+    
+    public double adjustMaxSpeed(double maxSpeed) {
+        return this.adjustSpeed(maxSpeed);
+    }
+    
+    public Sprite getCurrentSprite(Sprite sprite) {
+        return sprite;
+    }
+}
