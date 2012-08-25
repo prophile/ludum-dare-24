78e3749462c550362c6e69dc94761ec04bb61ae0
Sam Phippen <samphippen@googlemail.com>
2012-08-25 14:16:21 +0100

    Add the base entity class
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/Entity.java
@@ -0,0 +1,31 @@
+package me.teaisaweso.games.ld24;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+
+public class Entity {
+
+    protected Body mBody;
+
+    public Sprite getCurrentSprite() {
+        return null;
+    }
+
+    public Vector2 getPosition() {
+        Vector2 physicsPosition = mBody.getWorldCenter();
+        physicsPosition.mul(GameWrapper.PHYSICS_RATIO);
+        return physicsPosition;
+    }
+
+    public void update() {
+
+    }
+
+    public void draw(SpriteBatch sb) {
+        Sprite image = this.getCurrentSprite();
+        image.setPosition(this.getPosition().x, this.getPosition().y);
+        image.draw(sb);
+    }
+}
