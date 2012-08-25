dd03f7649c71b26b4e0f7189c22c0e9400c58785
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-08-25 16:46:45 +0100

    Boilerplate for physical and region obstacles

---
+++ inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/PhysicalObstacle.java
@@ -0,0 +1,11 @@
+package me.teaisaweso.games.ld24;
+
+public class PhysicalObstacle extends Obstacle {
+
+    @Override
+    public void hit() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
