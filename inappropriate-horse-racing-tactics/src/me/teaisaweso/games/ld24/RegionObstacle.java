a657baca0fb2332d67f3ea21cb2f4cbca8ffa3e5
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-08-25 16:59:04 +0100

    Add constructor for regionobstacle

--- inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/RegionObstacle.java
+++ inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/RegionObstacle.java
@@ -1,11 +1,19 @@
 package me.teaisaweso.games.ld24;
 
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
 public class RegionObstacle extends Obstacle {
-
-    @Override
-    public void hit() {
-        // TODO Auto-generated method stub
-
+    public RegionObstacle(World world, float x, float y, float width, float height) {
+        BodyDef bd = new BodyDef();
+        FixtureDef fd = new FixtureDef();
+        PolygonShape ps = new PolygonShape();
+        ps.setAsBox(width/(2*GameWrapper.PHYSICS_RATIO), height/(2*GameWrapper.PHYSICS_RATIO));
+        fd.isSensor = true;
+        bd.position.set(x/GameWrapper.PHYSICS_RATIO, y/GameWrapper.PHYSICS_RATIO);
+        mBody = world.createBody(bd);
+        mBody.createFixture(fd);
     }
-
 }
