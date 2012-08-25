13fcfb930bc88f0ca561674731178b3d1dd3b3fd
Sam Phippen <samphippen@googlemail.com>
2012-08-25 09:07:57 +0100

    Remove unused imports
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/GameWrapper.java
+++ inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/GameWrapper.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Matrix4;
 
 public class GameWrapper implements ApplicationListener {
