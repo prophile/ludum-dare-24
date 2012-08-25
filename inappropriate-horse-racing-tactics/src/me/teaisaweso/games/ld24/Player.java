6dc6ec1ce0b335551802b230a920a76931f0ac40
Sam Phippen <samphippen@googlemail.com>
2012-08-25 14:33:52 +0100

    Make player have physics lol
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/Player.java
@@ -0,0 +1,45 @@
+package me.teaisaweso.games.ld24;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+
+public class Player extends Entity {
+    private final List<StatusModifier> mStatusModifiers = new ArrayList<StatusModifier>();
+    private Sprite mSprite;
+    
+    public Player(Sprite sprite, World world) {
+        mSprite = sprite;
+        BodyDef bd = new BodyDef();
+        bd.fixedRotation = true;
+        bd.type = BodyType.StaticBody;
+        bd.fixedRotation = true;
+        bd.position.set(0,0);
+        Body b = world.createBody(bd);
+        mBody = b;
+    }
+    
+    
+    @Override
+    public Sprite getCurrentSprite() {
+        Sprite currentSprite = mSprite;
+        
+        for (StatusModifier modifier : mStatusModifiers) {
+            currentSprite = modifier.getCurrentSprite(currentSprite);
+        }
+        
+        return currentSprite;
+    }
+
+    @Override
+    public void update() {
+        super.update();
+    }
+
+}
