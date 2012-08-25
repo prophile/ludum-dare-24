d74907e846af222688c75bccbe47c5ac7a55010b
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-08-25 17:02:07 +0100

    Reformat

--- inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/RegionObstacle.java
+++ inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/RegionObstacle.java
@@ -5,14 +5,17 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
-public class RegionObstacle extends Obstacle {
-    public RegionObstacle(World world, float x, float y, float width, float height) {
+public abstract class RegionObstacle extends Obstacle {
+    public RegionObstacle(World world, float x, float y, float width,
+            float height) {
         BodyDef bd = new BodyDef();
         FixtureDef fd = new FixtureDef();
         PolygonShape ps = new PolygonShape();
-        ps.setAsBox(width/(2*GameWrapper.PHYSICS_RATIO), height/(2*GameWrapper.PHYSICS_RATIO));
+        ps.setAsBox(width / (2 * GameWrapper.PHYSICS_RATIO), height
+                / (2 * GameWrapper.PHYSICS_RATIO));
         fd.isSensor = true;
-        bd.position.set(x/GameWrapper.PHYSICS_RATIO, y/GameWrapper.PHYSICS_RATIO);
+        bd.position.set(x / GameWrapper.PHYSICS_RATIO, y
+                / GameWrapper.PHYSICS_RATIO);
         mBody = world.createBody(bd);
         mBody.createFixture(fd);
     }
