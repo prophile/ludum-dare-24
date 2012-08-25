4641b8e9ff0c33fa6527388c57d9f5c0a99e6f7d
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-08-25 17:02:42 +0100

    No need to override hit in PhysicalObstacle

--- inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/PhysicalObstacle.java
+++ inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/PhysicalObstacle.java
@@ -2,10 +2,4 @@ package me.teaisaweso.games.ld24;
 
 public class PhysicalObstacle extends Obstacle {
 
-    @Override
-    public void hit() {
-        // TODO Auto-generated method stub
-
-    }
-
 }
