e708b5c86aafa6cd0fa216dabc10d3e1c7345df1
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-08-26 17:54:08 +0100

    Fix some imports

--- inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/SoupObstacle.java
+++ inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/SoupObstacle.java
@@ -1,6 +1,7 @@
 package me.teaisaweso.games.ld24;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.Body;
