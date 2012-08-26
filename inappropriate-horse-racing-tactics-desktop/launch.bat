175fcba094c1f1f031c4d23a3f5938363d9f0779
Sam Phippen <samphippen@googlemail.com>
2012-08-26 14:59:46 +0100

    Add some launchers
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

---
+++ inappropriate-horse-racing-tactics-desktop/launch.bat
@@ -0,0 +1,8 @@
+../inappropriate-horse-racing-tactics-desktop/bin/
+../inappropriate-horse-racing-tactics-desktop/libs/gdx-backend-lwjgl.jar
+../inappropriate-horse-racing-tactics-desktop/libs/gdx-backend-lwjgl-natives.jar
+../inappropriate-horse-racing-tactics-desktop/libs/gdx-natives.jar
+../inappropriate-horse-racing-tactics/bin/
+../inappropriate-horse-racing-tactics/libs/gdx.jar
+"C:\Program Files (x86)\Java\jre6\bin\java" -cp "../inappropriate-horse-racing-tactics-desktop/bin/;../inappropriate-horse-racing-tactics-desktop/libs/gdx-backend-lwjgl.jar;../inappropriate-horse-racing-tactics-desktop/libs/gdx-backend-lwjgl-natives.jar;../inappropriate-horse-racing-tactics-desktop/libs/gdx-natives.jar;../inappropriate-horse-racing-tactics/bin/../inappropriate-horse-racing-tactics/libs/gdx.jar" me.teaisaweso.games.ld24.Main
+"C:\Program Files\Java\jre6\bin\java" -cp "../inappropriate-horse-racing-tactics-desktop/bin/;../inappropriate-horse-racing-tactics-desktop/libs/gdx-backend-lwjgl.jar;../inappropriate-horse-racing-tactics-desktop/libs/gdx-backend-lwjgl-natives.jar;../inappropriate-horse-racing-tactics-desktop/libs/gdx-natives.jar;../inappropriate-horse-racing-tactics/bin/../inappropriate-horse-racing-tactics/libs/gdx.jar" me.teaisaweso.games.ld24.Main
---
+++ inappropriate-horse-racing-tactics-desktop/launch.sh
@@ -0,0 +1,8 @@
+cd `dirname $0`
+java -cp "../inappropriate-horse-racing-tactics-desktop/bin/:\
+../inappropriate-horse-racing-tactics-desktop/libs/gdx-backend-lwjgl.jar:\
+../inappropriate-horse-racing-tactics-desktop/libs/gdx-backend-lwjgl-natives.jar:\
+../inappropriate-horse-racing-tactics-desktop/libs/gdx-natives.jar:\
+../inappropriate-horse-racing-tactics/bin/:\
+../inappropriate-horse-racing-tactics/libs/gdx.jar" me.teaisaweso.games.ld24.Main
+
