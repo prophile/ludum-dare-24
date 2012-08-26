0c71b355399b0793f90c4b359dacf9f36171aaf6
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-08-26 17:52:19 +0100

    Generate constructor and collide for soup

--- inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/SoupObstacle.java
+++ inappropriate-horse-racing-tactics/src/me/teaisaweso/games/ld24/SoupObstacle.java
@@ -2,6 +2,7 @@ package me.teaisaweso.games.ld24;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.Body;
 
 public class SoupObstacle extends PhysicalObstacle {
@@ -11,9 +12,12 @@ public class SoupObstacle extends PhysicalObstacle {
     }
 
     private static boolean sTexturesLoaded = false;
-    private static Texture sSoup1, sSoup2, sSoup3, sSoupTentacle1, sSoupTentacle2;
-    
+    private static Texture sSoup1, sSoup2, sSoup3, sSoupTentacle1,
+            sSoupTentacle2;
+
     private EvolutionStage mStage;
+    private Sprite mSprite;
+    private Sound mEvolutionSound;
 
     private static void loadTexturesOnDemand() {
         if (!sTexturesLoaded) {
@@ -26,18 +30,31 @@ public class SoupObstacle extends PhysicalObstacle {
         sSoup1 = new Texture(Gdx.files.internal("assets/Asset_Soup_1.png"));
         sSoup2 = new Texture(Gdx.files.internal("assets/Asset_Soup_2.png"));
         sSoup3 = new Texture(Gdx.files.internal("assets/Asset_Soup_3.png"));
-        sSoupTentacle1 = new Texture(Gdx.files.internal("assets/Asset_Soup_tentacle1.png"));
-        sSoupTentacle2 = new Texture(Gdx.files.internal("assets/Asset_Soup_tentacle2.png"));
+        sSoupTentacle1 = new Texture(Gdx.files
+                .internal("assets/Asset_Soup_tentacle1.png"));
+        sSoupTentacle2 = new Texture(Gdx.files
+                .internal("assets/Asset_Soup_tentacle2.png"));
     }
 
     public SoupObstacle(Body b) {
         super(b);
+        loadTexturesOnDemand();
+        mWidth = 180;
+        mHeight = 115;
+        mSprite = new Sprite(sSoup1, (int) mWidth, (int) mHeight);
+        mSprite.setOrigin(0.0f, 0.0f);
+        mSprite.setScale(1.0f);
+        mEvolutionSound = Gdx.audio.newSound(Gdx.files
+                .internal("assets/Evolve.wav"));
     }
-    
+
     @Override
     public void collide(Entity e) {
-        // TODO Auto-generated method stub
-
+        if (e instanceof Enemy && mStage != EvolutionStage.NORMAL) {
+            Enemy enemy = (Enemy) e;
+            enemy.addStatusModifier(freshStatusModifier());
+            enemy.mBody.setLinearVelocity(0.0f, 0.0f);
+        }
     }
 
     @Override
