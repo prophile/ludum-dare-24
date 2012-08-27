147e7ec0217f00468bd83b15501d6a13b1570b43
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-08-27 16:33:00 +0100

    Make macs happier

--- make-game-zip.sh
+++ make-game-zip.sh
@@ -3,7 +3,7 @@ dir=`mktemp -d lolwatXXXXXX`
 curdir=`pwd`
 echo $dir
 mkdir $dir/octodarwin
-find octodarwin* *.bat *.command | xargs -l1 -I@ cp -r @ $dir/octodarwin/
+find octodarwin* *.bat *.command | xargs -L 1 -I@ cp -r @ $dir/octodarwin/
 cd $dir
 zip octodarwin.zip octodarwin
 mv octodarwin.zip $curdir
