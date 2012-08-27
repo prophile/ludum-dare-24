c21196e500958c26e4fd5ece72eefb5c61436eed
Jeremy Morse <jmorse+git@studentrobotics.org>
2012-08-27 16:27:00 +0100

    Make game zip generator less crap

--- make-game-zip.sh
+++ make-game-zip.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-find octodarwin* *.bat *.command | xargs zip octodarwin.zip
+dir=`mktemp -d`
+curdir=`pwd`
+echo $dir
+mkdir $dir/octodarwin
+find octodarwin* *.bat *.command | xargs -l1 -I@ cp -r @ $dir/octodarwin/
+cd $dir
+zip octodarwin.zip octodarwin
+mv octodarwin.zip $curdir
+cd $curdir
+rm -rf $dir
